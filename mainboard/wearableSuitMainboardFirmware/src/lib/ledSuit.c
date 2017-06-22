beb53c2677796bd50d873803bd9e4925ca69f986
KekseJL <jcl02@gmx.de>
2017-06-22 21:26:15 +0200

    added 4 channel support to ws2812 library, refactored ledSuit library (reverted from commit 1cc2ef613847d8b94c4e8f30ec4543500a34e13e) (reverted from commit d02fd2d8e26421906d317fddd71ffd60a5ddc54a)

--- mainboard/wearableSuitMainboardFirmware/src/lib/ledSuit.c
+++ mainboard/wearableSuitMainboardFirmware/src/lib/ledSuit.c
@@ -9,25 +9,13 @@
 #include "ws2812.h"
 
 
-// Length of arms and legs
-#define ARM_LENGTH 2
-#define LEG_LENGTH 1
-// Length, channel and start index of the LED strips of the left arm
-#define LEFT_ARM_CHANNEL 1
+// Lengths and start indices of the LED strips of each body part
+#define ARM_LENGTH 3
 #define LEFT_ARM_START 0
-#define LEFT_ARM_LENGTH ARM_LENGTH
-// Length, channel and start index of the LED strips of the right arm
-#define RIGHT_ARM_CHANNEL 1
 #define RIGHT_ARM_START 3
-#define RIGHT_ARM_LENGTH ARM_LENGTH
-// Length, channel and start index of the LED strips of the left leg
-#define LEFT_LEG_CHANNEL 1
-#define LEFT_LEG_START 2
-#define LEFT_LEG_LENGTH LEG_LENGTH
-// Length, channel and start index of the LED strips of the right leg
-#define RIGHT_LEG_CHANNEL 1
-#define RIGHT_LEG_START 5
-#define RIGHT_LEG_LENGTH LEG_LENGTH
+#define LEG_LENGTH 3
+#define LEFT_LEG_START 0
+#define RIGHT_LEG_START 3
 
 
 // Arm structure
@@ -68,11 +56,11 @@ void _updateLeftArm()
 {
 	if (_leftArm.enabled)
 	{
-		ws2812_updateLeds(LEFT_ARM_CHANNEL, LEFT_ARM_START, LEFT_ARM_LENGTH, _leftArm.leds);
+		ws2812_updateLeds(LEFT_ARM_START, ARM_LENGTH, _leftArm.leds);
 	}
 	else
 	{
-		ws2812_switchLedsOff(LEFT_ARM_CHANNEL, LEFT_ARM_START, LEFT_ARM_LENGTH);
+		ws2812_switchLedsOff(LEFT_ARM_START, ARM_LENGTH);
 	}
 }
 
@@ -81,37 +69,11 @@ void _updateRightArm()
 {
 	if (_rightArm.enabled)
 	{
-		ws2812_updateLeds(RIGHT_ARM_CHANNEL, RIGHT_ARM_START, RIGHT_ARM_LENGTH, _rightArm.leds);
+		ws2812_updateLeds(RIGHT_ARM_START, ARM_LENGTH, _rightArm.leds);
 	}
 	else
 	{
-		ws2812_switchLedsOff(RIGHT_ARM_CHANNEL, RIGHT_ARM_START, RIGHT_ARM_LENGTH);
-	}
-}
-
-// Writes the configuration in the _leftLeg structure to the LED strip
-void _updateLeftLeg()
-{
-	if (_leftLeg.enabled)
-	{
-		ws2812_updateLeds(LEFT_LEG_CHANNEL, LEFT_LEG_START, LEFT_LEG_LENGTH, _leftLeg.leds);
-	}
-	else
-	{
-		ws2812_switchLedsOff(LEFT_LEG_CHANNEL, LEFT_LEG_START, LEFT_LEG_LENGTH);
-	}
-}
-
-// Writes the configuration in the _rightLeg structure to the LED strip
-void _updateRightLeg()
-{
-	if (_rightLeg.enabled)
-	{
-		ws2812_updateLeds(RIGHT_LEG_CHANNEL, RIGHT_LEG_START, RIGHT_LEG_LENGTH, _rightLeg.leds);
-	}
-	else
-	{
-		ws2812_switchLedsOff(RIGHT_LEG_CHANNEL, RIGHT_LEG_START, RIGHT_LEG_LENGTH);
+		ws2812_switchLedsOff(RIGHT_ARM_START, ARM_LENGTH);
 	}
 }
 
@@ -122,157 +84,133 @@ void ledSuit_init(void)
 	// Initialize WS2811 library
 	ws2812_init();
 
-	// Initialize suit
-	ledSuit_enableAll(1, 0); // Enable all body parts, do not update the LED strips
-	ledSuit_colorAllRgb(0, 0, 0, 0); // Initialize all body parts with color (0, 0, 0), do not update the LED strips
+	// Initialize arms
+	_leftArm.enabled = 1;
+	_rightArm.enabled = 1;
+	for (int i = 0; i < ARM_LENGTH; i++)
+	{
+		_leftArm.leds[i].r = 0;
+		_rightArm.leds[i].r = 0;
+		_leftArm.leds[i].g = 0;
+		_rightArm.leds[i].g = 0;
+		_leftArm.leds[i].b = 0;
+		_rightArm.leds[i].b = 0;
+	}
+
+	// Initialize legs
+	_leftLeg.enabled = 1;
+	_rightLeg.enabled = 1;
+	for (int i = 0; i < LEG_LENGTH; i++)
+	{
+		_leftLeg.leds[i].r = 0;
+		_rightLeg.leds[i].r = 0;
+		_leftLeg.leds[i].g = 0;
+		_rightLeg.leds[i].g = 0;
+		_leftLeg.leds[i].b = 0;
+		_rightLeg.leds[i].b = 0;
+	}
 }
 
-// Writes the configuration of a body part to the LED strip
-void ledSuit_updateBodyPart(bodyPart bodyPart)
+
+// Enables (enabled != 0) or disables (enabled == 1) the LEDs of the left arm
+void ledSuit_enableLeftArm(uint8_t enabled)
 {
-	switch (bodyPart)
-	{
-	case leftArm:
-		_updateLeftArm(); // Write the configuration in the _leftArm structure to the LED strip
-		break;
-	case rightArm:
-		_updateRightArm(); // Write the configuration in the _rightArm structure to the LED strip
-		break;
-	case leftLeg:
-		_updateLeftLeg(); // Write the configuration in the _leftLeg structure to the LED strip
-		break;
-	case rightLeg:
-		_updateRightLeg(); // Write the configuration in the _rightLeg structure to the LED strip
-		break;
-	default:
-		return;
-	}
+	// Update the enabled value in the _leftArm structure
+	_leftArm.enabled = enabled;
+
+	// Write the configuration in the _leftArm structure to the LED strip
+	_updateLeftArm();
 }
 
-// Writes the configuration of the whole suit to the LED strip
-void ledSuit_updateAll(void)
+// Enables (enabled != 0) or disables (enabled == 1) the LEDs of the right arm
+void ledSuit_enableRightArm(uint8_t enabled)
 {
-	// Update all body parts
-	for (int i = 0; i < bodyPartEnumSize; i++)
-	{
-		ledSuit_updateBodyPart(i);
-	}
+	// Update the enabled value in the _rightArm structure
+	_rightArm.enabled = enabled;
+
+	// Write the configuration in the _rightArm structure to the LED strip
+	_updateRightArm();
 }
 
-// Enables (enabled != 0) or disables (enabled == 0) the LEDs of a body part
-// Updates the LED strips only if (update != 0)
-void ledSuit_enableBodyPart(bodyPart bodyPart, uint8_t enabled, uint8_t update)
-{
-	switch (bodyPart)
-	{
-	case leftArm:
-		_leftArm.enabled = enabled; // Update the enabled value in the _leftArm structure
-		break;
-	case rightArm:
-		_rightArm.enabled = enabled; // Update the enabled value in the _rightArm structure
-		break;
-	case leftLeg:
-		_leftLeg.enabled = enabled; // Update the enabled value in the _leftLeg structure
-		break;
-	case rightLeg:
-		_rightLeg.enabled = enabled; // Update the enabled value in the _rightLeg structure
-		break;
-	default:
-		return;
-	}
-
-	if (update)
-	{
-		// Write the configuration of the body part to the LED strip
-		ledSuit_updateBodyPart(bodyPart);
-	}
-}
-
-// Enables (enabled != 0) or disables (enabled == 0) the LEDs of the whole suit
-// Updates the LED strips only if (update != 0)
-void ledSuit_enableAll(uint8_t enabled, uint8_t update)
+// Enables (enabled != 0) or disables (enabled == 1) the LEDs of a the whole suit
+void ledSuit_enableAll(uint8_t enabled)
 {
 	// Enable or disable all body parts
-	for (int i = 0; i < bodyPartEnumSize; i++)
-	{
-		ledSuit_enableBodyPart(i, enabled, update);
-	}
+	ledSuit_enableLeftArm(enabled);
+	ledSuit_enableRightArm(enabled);
 }
 
-// Colors a body part with an RGB value
-// Updates the LED strips only if (update != 0)
-void ledSuit_colorBodyPartRgb(bodyPart bodyPart, uint8_t red, uint8_t green, uint8_t blue, uint8_t update)
+
+// Colors the left arm with an RGB value
+void ledSuit_colorLeftArmRgb(uint8_t red, uint8_t green, uint8_t blue)
 {
-	switch (bodyPart)
+	// Update the color values in the _leftArm structure
+	for (int i = 0; i < ARM_LENGTH; i++)
 	{
-	case leftArm:
-		// Update the color values in the _leftArm structure
-		for (int i = 0; i < LEFT_ARM_LENGTH; i++)
-		{
-			_leftArm.leds[i].r = red;
-			_leftArm.leds[i].g = green;
-			_leftArm.leds[i].b = blue;
-		}
-		break;
-	case rightArm:
-		// Update the color values in the _rightArm structure
-		for (int i = 0; i < RIGHT_ARM_LENGTH; i++)
-		{
-			_rightArm.leds[i].r = red;
-			_rightArm.leds[i].g = green;
-			_rightArm.leds[i].b = blue;
-		}
-		break;
-	case leftLeg:
-		// Update the color values in the _leftLeg structure
-		for (int i = 0; i < LEFT_LEG_LENGTH; i++)
-		{
-			_leftLeg.leds[i].r = red;
-			_leftLeg.leds[i].g = green;
-			_leftLeg.leds[i].b = blue;
-		}
-		break;
-	case rightLeg:
-		// Update the color values in the _rightLeg structure
-		for (int i = 0; i < RIGHT_LEG_LENGTH; i++)
-		{
-			_rightLeg.leds[i].r = red;
-			_rightLeg.leds[i].g = green;
-			_rightLeg.leds[i].b = blue;
-		}
-		break;
-	default:
-		return;
+		_leftArm.leds[i].r = red;
+		_leftArm.leds[i].g = green;
+		_leftArm.leds[i].b = blue;
 	}
 
-	if (update)
+	// Write the color values in the _leftArm structure to the LED strip
+	_updateLeftArm();
+}
+
+// Colors the right arm with an RGB value
+void ledSuit_colorRightArmRgb(uint8_t red, uint8_t green, uint8_t blue)
+{
+	// Update the color values in the _rightArm structure
+	for (int i = 0; i < ARM_LENGTH; i++)
 	{
-		// Write the configuration of the body part to the LED strip
-		ledSuit_updateBodyPart(bodyPart);
+		_rightArm.leds[i].r = red;
+		_rightArm.leds[i].g = green;
+		_rightArm.leds[i].b = blue;
 	}
+
+	// Write the color values in the _rightArm structure to the LED strip
+	_updateRightArm();
+}
+
+
+// Colors the whole suit with an RGB value
+void ledSuit_colorAllRgb(uint8_t red, uint8_t green, uint8_t blue)
+{
+	// Color all body parts
+	ledSuit_colorLeftArmRgb(red, green, blue);
+	ledSuit_colorRightArmRgb(red, green, blue);
+}
+
+
+// Colors the left arm with a color
+void ledSuit_colorLeftArm(ledColor color)
+{
+	// Update the color values in the _leftArm structure
+	for (int i = 0; i < ARM_LENGTH; i++)
+	{
+			_leftArm.leds[i] = _colorLut[color];
+	}
+
+	// Write the color values in the _leftArm structure to the LED strip
+	_updateLeftArm();
+}
+
+// Colors the right arm with a color
+void ledSuit_colorRightArm(ledColor color)
+{
+	// Update the color values in the _rightArm structure
+	for (int i = 0; i < ARM_LENGTH; i++)
+	{
+		_rightArm.leds[i] = _colorLut[color];
+	}
+
+	// Write the color values in the _rightArm structure to the LED strip
+	_updateRightArm();
 }
 
 // Colors the whole suit with an RGB value
-// Updates the LED strips only if (update != 0)
-void ledSuit_colorAllRgb(uint8_t red, uint8_t green, uint8_t blue, uint8_t update)
+void ledSuit_colorAll(ledColor color)
 {
 	// Color all body parts
-	for (int i = 0; i < bodyPartEnumSize; i++)
-	{
-		ledSuit_colorBodyPartRgb(i, red, green, blue, update);
-	}
-}
-
-// Colors a body part with a color
-// Updates the LED strips only if (update != 0)
-void ledSuit_colorBodyPart(bodyPart bodyPart, ledColor color, uint8_t update)
-{
-	ledSuit_colorBodyPartRgb(bodyPart, _colorLut[color].r, _colorLut[color].g, _colorLut[color].b, update);
-}
-
-// Colors the whole suit with a color
-// Updates the LED strips only if (update != 0)
-void ledSuit_colorAll(ledColor color, uint8_t update)
-{
-	ledSuit_colorAllRgb(_colorLut[color].r, _colorLut[color].g, _colorLut[color].b, update);
+	ledSuit_colorLeftArm(color);
+	ledSuit_colorRightArm(color);
 }
--- mainboard/wearableSuitMainboardFirmware/src/main.c
+++ mainboard/wearableSuitMainboardFirmware/src/main.c
@@ -24,23 +24,33 @@ int main(void)
 {
 	ledSuit_init();
 
-	while (1)
+	for(;;)
 	{
-		ledSuit_colorBodyPart(leftArm, red, 1);
-		ledSuit_colorBodyPart(rightArm, green, 1);
-		ledSuit_colorBodyPart(leftLeg, blue, 1);
-		ledSuit_colorBodyPart(rightLeg, yellow, 1);
+		ledSuit_colorLeftArm(red);
+		ledSuit_colorRightArm(white);
 		Delay(1000);
-		ledSuit_colorBodyPartRgb(leftArm, 50, 50, 50, 1);
-		ledSuit_colorBodyPartRgb(rightArm, 50, 50, 50, 0);
+		ledSuit_colorLeftArm(orange);
 		Delay(1000);
-		ledSuit_updateAll();
+		ledSuit_colorLeftArm(yellow);
 		Delay(1000);
-		ledSuit_colorAll(white, 1);
+		ledSuit_colorLeftArm(green);
 		Delay(1000);
-		ledSuit_enableAll(0, 1);
+		ledSuit_colorLeftArm(teal);
 		Delay(1000);
-		ledSuit_enableAll(1, 1);
+		ledSuit_colorLeftArm(blue);
+		Delay(1000);
+		ledSuit_colorLeftArm(purple);
+		Delay(1000);
+		ledSuit_colorLeftArm(pink);
+		Delay(1000);
+		ledSuit_colorLeftArm(white);
+		Delay(1000);
+		ledSuit_colorLeftArmRgb(50, 50, 50);
+		ledSuit_colorRightArmRgb(50, 50, 50);
+		Delay(1000);
+		ledSuit_enableLeftArm(0);
+		Delay(1000);
+		ledSuit_enableLeftArm(1);
 		Delay(1000);
 	}
 }
