5dd55ea46ed9b0f1813ed1112003ad88eac31961
KekseJL <jcl02@gmx.de>
2017-06-22 21:35:10 +0200

    added 4 channel support to ws2812 library, refactored ledSuit library (reverted from commit 1cc2ef613847d8b94c4e8f30ec4543500a34e13e) (reverted from commit d02fd2d8e26421906d317fddd71ffd60a5ddc54a) (reverted from commit beb53c2677796bd50d873803bd9e4925ca69f986)

--- mainboard/wearableSuitMainboardFirmware/src/lib/ws2812.h
+++ mainboard/wearableSuitMainboardFirmware/src/lib/ws2812.h
@@ -11,7 +11,15 @@
 #include "stm32f4xx.h"
 
 
-#define LED_COUNT 6 // Number of LEDs
+#define ENABLE_CHANNEL_1 // Enable ws2812 channel 1 on pin A6
+//#define ENABLE_CHANNEL_2 // Enable ws2812 channel 2 on pin A7
+//#define ENABLE_CHANNEL_3 // Enable ws2812 channel 3 on pin B0
+//#define ENABLE_CHANNEL_4 // Enable ws2812 channel 4 on pin B1
+
+#define LED_COUNT_CH1 6 // Number of LEDs in channel 1
+#define LED_COUNT_CH2 6 // Number of LEDs in channel 2
+#define LED_COUNT_CH3 6 // Number of LEDs in channel 3
+#define LED_COUNT_CH4 6 // Number of LEDs in channel 4
 
 
 // RGB LED structure
@@ -26,21 +34,21 @@ typedef struct
 // Initializes the ws2812 (all LEDs switched off)
 void ws2812_init(void);
 
-// Updates a number of LEDs beginning at the start address
-void ws2812_updateLeds(uint16_t startAddress, uint16_t ledCount, rgbLed * ledData);
+// Updates a number of LEDs of a channel beginning at the start address
+void ws2812_updateLeds(uint8_t channel, uint16_t startAddress, uint16_t ledCount, rgbLed * ledData);
 
-// Sets a number of LEDs to RGB value beginning at the start address
-void ws2812_setLedsRGB(uint16_t startAddress, uint16_t ledCount, uint8_t r, uint8_t g, uint8_t b);
+// Sets a number of LEDs of a channel to RGB value beginning at the start address
+void ws2812_setLedsRGB(uint8_t channel, uint16_t startAddress, uint16_t ledCount, uint8_t r, uint8_t g, uint8_t b);
 // Sets all LEDs to RGB value
 void ws2812_setAllLedsRGB(uint8_t r, uint8_t g, uint8_t b);
 
-// Sets a number of LEDs to a color beginning at the start address
-void ws2812_setLedsToColor(uint16_t startAddress, uint16_t ledCount, rgbLed color);
+// Sets a number of LEDs of a channel to a color beginning at the start address
+void ws2812_setLedsToColor(uint8_t channel, uint16_t startAddress, uint16_t ledCount, rgbLed color);
 // Sets all LEDs to a color
 void ws2812_setAllLedsToColor(rgbLed color);
 
-// switches a number of LEDs off beginning at the start address
-void ws2812_switchLedsOff(uint16_t startAddress, uint16_t ledCount);
+// switches a number of LEDs of a channel off beginning at the start address
+void ws2812_switchLedsOff(uint8_t channel, uint16_t startAddress, uint16_t ledCount);
 // switches all LEDs off
 void ws2812_switchAllLedsOff(uint16_t startAddress, uint16_t ledCount);
 
