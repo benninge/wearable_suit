0a7b9277575d27a919ccfdd62e061403b699ee2f
KekseJL <jcl02@gmx.de>
2017-07-06 20:36:47 +0200

    Implemented basic microphone library functionality

--- mainboard/wearableSuitMainboardFirmware/src/lib/mic.c
+++ mainboard/wearableSuitMainboardFirmware/src/lib/mic.c
@@ -5,20 +5,24 @@
  *      Author: kekse
  */
 
-
 #include "mic.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
-#include "stm32f4xx.h"
-
 #include "arm_math.h"
 #include "arm_common_tables.h"
 
 
-#define DMA_BUFFER_LENGTH 128
+#define NUMBER_OF_SAMPLES 2048 // Array size of sample buffer
+
+
+// FFT instance, FFT size should be half of number of samples
+const arm_cfft_instance_f32 _fftInstance = {1024, twiddleCoef_1024, armBitRevIndexTable1024, ARMBITREVINDEXTABLE1024_TABLE_LENGTH};
+// Indexes which are used for generating a spectrum from the FFT output
+const uint16_t _spectrumIndexes[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 511};
+
 
 // Global variables
-uint16_t dmaBuffer[DMA_BUFFER_LENGTH]; // Array containing a stream of ADC samples
+uint16_t _sampleBuffer[NUMBER_OF_SAMPLES]; // Array containing a stream of ADC samples
 
 
 // Initializes the microphone library
@@ -37,6 +41,7 @@ void mic_init(void)
 	gpioInitStructure.GPIO_Mode = GPIO_Mode_OUT; // GPIO as output
 	GPIO_Init(GPIOC, &gpioInitStructure); // Initialize GPIO C
 
+	// Set microphone configuration pins
 	GPIO_SetBits(GPIOC, GPIO_Pin_1 & GPIO_Pin_2); // 1:2000 A/R ratio , 40dB gain
 
 	// ADC initialization
@@ -45,67 +50,103 @@ void mic_init(void)
 	ADC_StructInit(&adcInitStructure); // Initialize ADC initialization structure with default values
 	adcInitStructure.ADC_ContinuousConvMode = ENABLE; // Continuous conversion
 	ADC_Init(ADC1, &adcInitStructure); // Initialize ADC
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_480Cycles); // Configure ADC channel
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_112Cycles); // Configure ADC channel
 	ADC_Cmd(ADC1, ENABLE); // Enable ADC
 
-	// DMA initialization
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE); // Enable clock for DMA
-	DMA_InitTypeDef dmaInitStructure;
-	DMA_StructInit(&dmaInitStructure); // Initialize DMA initialization structure with default values
-	dmaInitStructure.DMA_Channel = DMA_Channel_0; // Use DMA channel 0
-	dmaInitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR; // Store data from ADC 1 data register
-	dmaInitStructure.DMA_Memory0BaseAddr = (uint32_t)dmaBuffer; // Write data to DMA buffer
-	dmaInitStructure.DMA_BufferSize = (uint32_t)DMA_BUFFER_LENGTH; // DMA buffer size = length of DMA buffer
-	dmaInitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable; // Auto increment memory address
-	dmaInitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord; // 16 bit DMA data size
-	dmaInitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord; // 16 bit memory data size
-	dmaInitStructure.DMA_Mode = DMA_Mode_Circular; // Circular buffer mode
-	//dmaInitStructure.DMA_Priority = DMA_Priority_High; // High DMA priority
-	DMA_Init(DMA2_Stream0, &dmaInitStructure); // Initialize DMA stream 0
-	DMA_Cmd(DMA2_Stream0, ENABLE); // Enable DMA stream 0
-	while (DMA_GetCmdStatus(DMA2_Stream0) != ENABLE); // Wait for DMA to become enabled
-	ADC_DMACmd(ADC1, ENABLE); // Enable ADC DMA request
+	// Start ADC conversion
+	ADC_SoftwareStartConv(ADC1);
 
-	ADC_SoftwareStartConv(ADC1); // Start ADC conversion
+	// Initialize timer for ADC sampling
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE); // Enable clock for timer
+	TIM_TimeBaseInitTypeDef  timerInitStructure;
+	TIM_TimeBaseStructInit(&timerInitStructure); // Initialize timer initialization structure with default values
+	timerInitStructure.TIM_Period = SystemCoreClock / 40000 - 1;  // 40 kHz sampling rate = 20 kHz max frequency
+	TIM_TimeBaseInit(TIM4, &timerInitStructure); // Initialize timer
+	// Initialize timer interrupt
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn; // Timer 4 interrupt
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 5; // Medium priority
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 5; // Medium priority
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; // Enable interrupt
+	NVIC_Init(&NVIC_InitStructure); // Initialize timer interrupt
+	TIM_ClearITPendingBit(TIM4, TIM_IT_Update); // Clear interrupt flag
+	TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE); // Enable timer update interrupt
+
+	// Start timer for ADC sampling
+	TIM_Cmd(TIM4, ENABLE);
 }
 
-const arm_cfft_instance_f32 CFFT_Instances[] = {
-	{16, twiddleCoef_16, armBitRevIndexTable16, ARMBITREVINDEXTABLE__16_TABLE_LENGTH},
-	{32, twiddleCoef_32, armBitRevIndexTable32, ARMBITREVINDEXTABLE__32_TABLE_LENGTH},
-	{64, twiddleCoef_64, armBitRevIndexTable64, ARMBITREVINDEXTABLE__64_TABLE_LENGTH},
-	{128, twiddleCoef_128, armBitRevIndexTable128, ARMBITREVINDEXTABLE_128_TABLE_LENGTH},
-	{256, twiddleCoef_256, armBitRevIndexTable256, ARMBITREVINDEXTABLE_256_TABLE_LENGTH},
-	{512, twiddleCoef_512, armBitRevIndexTable512, ARMBITREVINDEXTABLE_512_TABLE_LENGTH},
-	{1024, twiddleCoef_1024, armBitRevIndexTable1024, ARMBITREVINDEXTABLE1024_TABLE_LENGTH},
-	{2048, twiddleCoef_2048, armBitRevIndexTable2048, ARMBITREVINDEXTABLE2048_TABLE_LENGTH},
-	{4096, twiddleCoef_4096, armBitRevIndexTable4096, ARMBITREVINDEXTABLE4096_TABLE_LENGTH}
-};
-
-// Only for testing purposes
-void mic_test(void)
+// Gets the spectrum of the current sound recorded by the microphone, The array size of "spectrum" must be 10
+void mic_getSpectrum(uint8_t * spectrum)
 {
-	const arm_cfft_instance_f32 * fft = &CFFT_Instances[2];
+	const arm_cfft_instance_f32 * fft = &_fftInstance; // FFT instance pointer
 
-	float32_t Input[DMA_BUFFER_LENGTH];   /*!< Input buffer is always 2 * FFT_SIZE */
-	float32_t Output[DMA_BUFFER_LENGTH / 2]; /*!< Output buffer is always FFT_SIZE */
+	float32_t fftInput[NUMBER_OF_SAMPLES]; // FFT input array
+	float32_t fftOutput[NUMBER_OF_SAMPLES / 2]; // FFT output array
 
-	for (int i = 0; i < DMA_BUFFER_LENGTH; i++)
+	// Fill FFT input array with microphone measurements
+	for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
 	{
-		Input[i] = (float32_t)dmaBuffer[i] - 1750;
+		fftInput[i] = (float32_t)_sampleBuffer[i] - 1750;
 	}
 
-	float32_t MaxValue;
-	uint32_t MaxIndex;
+	// Run FFT on microphone measurement
+	arm_cfft_f32(fft, fftInput, 0, 1);
+	arm_cmplx_mag_f32(fftInput, fftOutput, NUMBER_OF_SAMPLES / 2);
 
-	arm_cfft_f32(fft, Input, 0, 1);
-	arm_cmplx_mag_f32(Input, Output, DMA_BUFFER_LENGTH / 2);
-	arm_max_f32(Output, DMA_BUFFER_LENGTH / 2, &MaxValue, &MaxIndex);
+	// Calculate the first spectrum data point from the FFT output
+	if((uint16_t)(fftOutput[1] / 1000) > 255) // Amplitude of the frequency range is above the uint8 value range
+	{
+		spectrum[0] = 255; // Set data point to max value
+	}
+	else
+	{
+		spectrum[0] = (uint16_t)(fftOutput[1] / 1000);
+	}
+
+	// Calculate the remaining spectrum data points from the FFT output
+	for (uint8_t i = 0; i < 9; i++)
+	{
+		// find the frequency with the highest amplitude in the frequency range
+		uint16_t max = 0;
+		for (uint16_t j = _spectrumIndexes[i] + 1; j <= _spectrumIndexes[i + 1]; j++)
+		{
+			if ((fftOutput[j] / 1000) > max)
+			{
+				max = (uint16_t)(fftOutput[j] / 1000);
+			}
+		}
+
+		if (max > 255) // Amplitude of the frequency range is above the uint8 value range
+		{
+			spectrum[i + 1] = 255; // Set data point to max value
+		}
+		else
+		{
+			spectrum[i + 1] = max;
+		}
+	}
+}
 
 
-	//uint16_t value = ADC_GetConversionValue(ADC1);
-	//DMA_Cmd(DMA2_Stream0, DISABLE); // Enable DMA stream 0
-	//DMA_Cmd(DMA2_Stream0, ENABLE); // Enable DMA stream 0
-	//ADC_DMACmd(ADC1, ENABLE); // Enable ADC DMA request
-	//ADC_SoftwareStartConv(ADC1); // Start ADC conversion
-	return;
+// Timer 4 interrupt handler
+void TIM4_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM4, TIM_IT_Update)) // Timer update interrupt
+	{
+		static uint16_t bufferIndex = 0;
+
+		_sampleBuffer[bufferIndex] = ADC_GetConversionValue(ADC1); // Write current ADC sample to buffer
+
+		if (bufferIndex >= NUMBER_OF_SAMPLES) // End of buffer reached
+		{
+			bufferIndex = 0; // Reset buffer index
+		}
+		else
+		{
+			bufferIndex++; // Increment buffer index
+		}
+
+	    TIM_ClearITPendingBit(TIM4, TIM_IT_Update); // Clear interrupt flag
+	}
 }
--- mainboard/wearableSuitMainboardFirmware/src/lib/mic.h
+++ mainboard/wearableSuitMainboardFirmware/src/lib/mic.h
@@ -8,12 +8,14 @@
 #ifndef LIB_MIC_H_
 #define LIB_MIC_H_
 
+#include "stm32f4xx.h"
+
 
 // Initializes the microphone library
 void mic_init(void);
 
-// Only for testing purposes
-void mic_test(void);
+// Gets the spectrum of the current sound recorded by the microphone, The array size of "spectrum" must be 10
+void mic_getSpectrum(uint8_t * spectrum);
 
 
 #endif /* LIB_MIC_H_ */
