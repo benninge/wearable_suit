6d580186b990a9b45a741866318255fbf4b72c84
KekseJL <jcl02@gmx.de>
2017-07-20 15:08:18 +0200

    added some example main code

--- mainboard/wearableSuitMainboardFirmware/src/main.c
+++ mainboard/wearableSuitMainboardFirmware/src/main.c
@@ -13,6 +13,9 @@
 
 #include "lib/ledSuit.h"
 #include "lib/rs485.h"
+#include "lib/bluetooth.h"
+#include "lib/mic.h"
+
 
 void Delay2(uint32_t time)
 {
@@ -20,45 +23,42 @@ void Delay2(uint32_t time)
 	while(time--);
 }
 
+
 int main(void)
 {
 	SystemInit();
 
-	//ledSuit_init();
-	rs485_init(38400);
+	mic_init();
+	ledSuit_init();
 
-	/*rgbLed fadeColors[4] = {
-				{ .r = 255, .g = 0, .b = 0 },
-				{ .r = 0, .g = 255, .b = 0 },
-				{ .r = 0, .g = 0, .b = 255 },
-				{ .r = 255, .g = 0, .b = 0 } };
+	rgbLed fadeColors[4] = {
+			{ .r = 255, .g = 0, .b = 0 },
+			{ .r = 0, .g = 255, .b = 0 },
+			{ .r = 0, .g = 0, .b = 255 },
+			{ .r = 255, .g = 0, .b = 0 } };
 
-	rgbLed pulseColors[5] = {
-				{ .r = 10, .g = 0, .b = 0 },
-				{ .r = 50, .g = 0, .b = 0 },
-				{ .r = 255, .g = 0, .b = 0 },
-				{ .r = 50, .g = 0, .b = 0 },
-				{ .r = 10, .g = 0, .b = 0 } };
+	ledSuit_colorFade(WHOLE_SUIT & ~CHEST, 4, fadeColors, 1);
+	ledSuit_configureAutoRotate(WHOLE_SUIT & ~CHEST, forwards, 240);
+	ledSuit_enableAutoRotate(WHOLE_SUIT & ~CHEST, 1);
 
-	ledSuit_color(LEFT_LEG, 0, 0, white, 1);
-	ledSuit_setStrobePeriod(1);
-	ledSuit_enableStrobe(LEFT_LEG, 1);
+	while (1)
+	{
+		//Delay2(100);
 
-	ledSuit_configureAutoColorFade(RIGHT_LEG, 3, fadeColors, 255);
-	ledSuit_enableAutoColorFade(RIGHT_LEG, 1);
+		uint8_t spectrum[10];
+		mic_getSpectrum(spectrum, 200);
 
-	ledSuit_colorFade(RIGHT_ARM, 4, fadeColors, 1);
-	ledSuit_color(LEFT_ARM, 0, 0, black, 0);
-	ledSut_colorArray(LEFT_ARM, 0, 5, pulseColors, 1);
-	ledSuit_configureAutoRotate(RIGHT_ARM + LEFT_ARM, forwards, 255);
-	ledSuit_enableAutoRotate(RIGHT_ARM + LEFT_ARM, 1);*/
-
-	while(1) {
-		rs485_requestSensorData(leftArmSensor);
-		Delay2(100);
-		rs485_updateSensorData(leftArmSensor);
-		//Delay(33);
+		ledSuit_displaySpectrum(spectrum, 1);
 	}
+
+	//rs485_init(38400);
+
+	//while(1) {
+		//rs485_requestSensorData(leftArmSensor);
+		//Delay2(100);
+		//rs485_updateSensorData(leftArmSensor);
+		//Delay(33);
+	//}
 }
 
 
