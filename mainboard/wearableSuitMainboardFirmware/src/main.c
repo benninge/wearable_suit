bad08e80dc75970ec4933a7396948f9ee01e05bc
KekseJL <jcl02@gmx.de>
2017-07-29 19:10:45 +0200

    improved getMoving function

--- mainboard/wearableSuitMainboardFirmware/src/main.c
+++ mainboard/wearableSuitMainboardFirmware/src/main.c
@@ -30,6 +30,7 @@ int main(void)
 
 	mic_init();
 	ledSuit_init();
+	rs485_init(57600);
 
 	rgbLed fadeColors[4] = {
 			{ .r = 255, .g = 0, .b = 0 },
@@ -37,18 +38,39 @@ int main(void)
 			{ .r = 0, .g = 0, .b = 255 },
 			{ .r = 255, .g = 0, .b = 0 } };
 
-	ledSuit_colorFade(WHOLE_SUIT & ~CHEST, 4, fadeColors, 1);
-	ledSuit_configureAutoRotate(WHOLE_SUIT & ~CHEST, forwards, 240);
-	ledSuit_enableAutoRotate(WHOLE_SUIT & ~CHEST, 1);
+	//ledSuit_colorFade(WHOLE_SUIT & ~CHEST, 4, fadeColors, 1);
+	//ledSuit_configureAutoRotate(WHOLE_SUIT & ~CHEST, forwards, 240);
+	//ledSuit_enableAutoRotate(WHOLE_SUIT & ~CHEST, 1);
 
-	while (1)
+	ledSuit_colorFade(WHOLE_SUIT, 4, fadeColors, 1);
+	ledSuit_configureAutoRotate(WHOLE_SUIT, forwards, 240);
+	ledSuit_enableAutoRotate(WHOLE_SUIT, 1);
+
+	while (1);
 	{
 		//Delay2(100);
 
-		uint8_t spectrum[10];
-		mic_getSpectrum(spectrum, 200);
+		//uint8_t spectrum[10];
+		//mic_getSpectrum(spectrum, 230);
+		/*uint16_t audioLevel = mic_getAudioLevel();
+		audioLevel /= 100;
+		if (audioLevel < 10) audioLevel = 0;
+		else audioLevel -= 10;*/
 
-		ledSuit_displaySpectrum(spectrum, 1);
+		rs485_updateSensorData();
+
+		uint8_t leftArmMoving = rs485_getMoving(leftArmSensor);
+		uint8_t rightArmMoving = rs485_getMoving(rightArmSensor);
+		uint8_t leftLegMoving = rs485_getMoving(leftLegSensor);
+		uint8_t rightLegMoving = rs485_getMoving(rightLegSensor);
+
+		ledSuit_enable(LEFT_ARM, leftArmMoving, 1);
+		ledSuit_enable(RIGHT_ARM, rightArmMoving, 1);
+		ledSuit_enable(LEFT_LEG, leftLegMoving, 1);
+		ledSuit_enable(RIGHT_LEG, rightLegMoving, 1);
+
+		//ledSuit_setBrightness(WHOLE_SUIT, audioLevel, 1);
+		//ledSuit_displaySpectrum(spectrum, 1);
 	}
 
 	//rs485_init(38400);
